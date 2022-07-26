718718d5b74968a765ed5290fcec12abc3cba411
Malachy Ekene Ezema <52065233+ekene12e@users.noreply.github.com>
2022-07-26 21:49:04 +0100

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join
 
+matched = ARGV[0].scan(/(\[from:.*?\]|\[to:.*?\]|\[flags:.*?\])/)
+
+res = Array.new
+
+for match in matched do 
+   matched_string = match.join
+   res.append(matched_string[matched_string.index(':') + 1..-2])
+end
+
+puts res.join(',')
