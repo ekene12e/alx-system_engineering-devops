61348efcbaa304f94dd8f1f25d55a7e4f57b92cb
ekene12e <ekenestanekeson@gmail.com>
2022-11-06 10:41:44 +0100

    task 1

--- 0x16-api_advanced/test.py
+++ 0x16-api_advanced/test.py
@@ -11,8 +11,13 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
+    print(response.status_code)
     if response.status_code == 200:
         data = response.json()
+        print(data)
         return data.get('data').get('subscribers')
     else:
         return 0
+
+if __name__ == '__main__':
+    print(number_of_subscribers('sex'))
